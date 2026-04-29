c1f7ee9fe31d16fa51593280ef9dd3d9e0d11a59
StephenTetteh-Nirvana <stephentetteh2616@gmail.com>
2026-04-29 10:44:52 +0000

    users can now choose between phone Number or email for login

--- components/home/FAQ.tsx
+++ components/home/FAQ.tsx
@@ -6,36 +6,31 @@ import {
 } from "@/components/ui/accordion"
 
 import { items } from "@/FAQ"
-import { BadgeQuestionMark, CircleQuestionMark } from "lucide-react"
 
 
 const FAQ = () => {
   return (
     <div className="mt-15 w-full flex flex-col gap-5 items-center justify-center pb-10">
-        {/* Heading  */}
-        <h2 className="text-3xl font-bold text-center mb-8 text-[var(--primaryTheme)]">Got Questions?</h2>
+      {/* Heading  */}
+      <h2 className="text-3xl font-bold text-center mb-8 text-[var(--primaryTheme)]">Got Questions? We've Got Answers</h2>
 
-        {/* Q & As  */}
-        <div className="w-[97%] md:w-[70%] px-4">
-
+      {/* Q & As  */}
+      <div className="w-[97%] md:w-[70%] px-4">
         <Accordion
           type="single"
           collapsible
           className="mx-auto -mt-10"
         >
-            {items.map((item,idx) => (
-                <div key={idx} className="bg-slate-100 mt-5 px-5 py-2 rounded-[8px] flex items-center gap-2">
-                  <div className="">
-                    <CircleQuestionMark />
-                  </div>
-                  <AccordionItem key={item.value} value={item.value} className="flex flex-col flex-1">
-                    <AccordionTrigger className="cursor-pointer">{item.trigger}</AccordionTrigger>
-                    <AccordionContent>{item.content}</AccordionContent>
-                  </AccordionItem>
-                </div>
-            ))}
-          </Accordion>
-        </div>
+          {items.map((item,idx) => (
+            <div key={idx} className="bg-slate-100 mt-5 px-5 py-2 rounded-[8px] flex items-center gap-2">
+              <AccordionItem key={item.value} value={item.value} className="flex flex-col flex-1">
+                <AccordionTrigger className="cursor-pointer">{item.trigger}</AccordionTrigger>
+                <AccordionContent>{item.content}</AccordionContent>
+              </AccordionItem>
+            </div>
+          ))}
+        </Accordion>
+      </div>
     </div>
 
   )
--- components/home/RegisterCard.tsx
+++ components/home/RegisterCard.tsx
@@ -19,78 +19,102 @@ import Link from "next/link";
 import { useState } from "react";
 
 const RegisterCard = () => {
-    const [optionSelected,setOptionSelected] = useState("email")
-
+  const [optionSelected,setOptionSelected] = useState("email")
 
     return (
-        <Card className="w-full max-w-sm">
-          {/* Navigate back to homepage  */}
-          <Link href="/">
-            <div className="flex items-center justify-center gap-2 cursor-pointer">
-              <GraduationCap size={25} className="text-[var(--primaryTheme)]"/>
-              <h1 className="text-center font-[900] text-[var(--primaryTheme)] text-[18px]">EDUSCRIBE</h1> 
-            </div>
-          </Link>
+      <Card className="w-full max-w-sm">
+        {/* Navigate back to homepage  */}
+        <Link href="/">
+          <div className="flex items-center justify-center gap-2 cursor-pointer">
+            <GraduationCap size={25} className="text-[var(--primaryTheme)]"/>
+            <h1 className="text-center font-[900] text-[var(--primaryTheme)] text-[18px]">EDUSCRIBE</h1> 
+          </div>
+        </Link>
 
-          <CardHeader>
-            <CardTitle>Create a new account</CardTitle>
-            <CardDescription>
-              Enter your details below to create an account
-            </CardDescription>
-          </CardHeader>
+        <CardHeader>
+          <CardTitle>Create a new account</CardTitle>
+          <CardDescription>
+            Enter your details below to create an account
+          </CardDescription>
+        </CardHeader>
 
-          <CardContent>
-            <form>
-              <div className="flex flex-col gap-6">
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              {optionSelected === "email" ? (
                 <div className="grid gap-2">
                   <div className={`${optionSelected === "email" ? "flex items-center justify-between" : "hidden"}`}>
                     <Label htmlFor="email">Email</Label>
                     <CardAction 
-                      className="text-[var(--primaryTheme)] font-[700] cursor-pointer">Use phone number instead ? 
+                      className="text-[var(--primaryTheme)] font-[700] cursor-pointer"
+                    >
+                      <Button 
+                        variant="ghost" 
+                        className="cursor-pointer"
+                        onClick={()=>setOptionSelected("phoneNumber")}
+                      >
+                        Use phone number instead ? 
+                      </Button>
                     </CardAction>
                   </div>
-                  <Input
-                    id="email"
-                    type="email"
-                    placeholder="m@example.com"
-                    required
+                  <Input 
+                    id="phoneNumber" 
+                    type="number" 
+                    required 
+                    className="py-5 border border-slate-300" 
                   />
                 </div>
-                <div className="grid gap-2 hidden">
+              ) : (
+                <div className="grid gap-2">
                   <div className={`${optionSelected === "phoneNumber" ? "flex items-center justify-between" : "hidden"}`}>
                     <Label htmlFor="phoneNumber">Phone Number</Label>
                     <CardAction 
-                      onClick={()=>setOptionSelected("phoneNumber")}
-                      className="text-[var(--primaryTheme)] font-[700] cursor-pointer">Use email instead ? 
+                      className="text-[var(--primaryTheme)] font-[700] cursor-pointer"
+                    >
+                      <Button 
+                        variant="ghost" 
+                        className="cursor-pointer"
+                        onClick={()=>setOptionSelected("phoneNumber")}
+                      >
+                        Use email instead ? 
+                      </Button>
                     </CardAction>
                   </div>
-                  <Input id="phoneNumber" type="phoneNumber" required />
+                  <Input 
+                    id="phoneNumber" 
+                    type="number" 
+                    required 
+                    className="py-5 border border-slate-300" 
+                  />
                 </div>
-              </div>
-            </form>
-          </CardContent>
+              )}
 
-          <CardFooter className="flex-col gap-2">
-            <Button type="submit" className="w-full py-5 cursor-pointer bg-[var(--secondaryTheme)]">
-              Register
-            </Button>
-            <Button variant="outline" className="w-full flex items-center gap-2 cursor-pointer py-5">
-              <FcGoogle />
-              Continue with Google
-            </Button>
-            <Button variant="outline" className="w-full cursor-pointer flex items-center gap-2 py-5">
-              <FaFacebook />
-              Continue with Facebook
-            </Button>
 
-            {/* Link to sign up  */}
-            <h2 className="mt-3">Already got an account? 
-                <Link href="/login">
-                  <span className="text-[var(--primaryTheme)] font-[700]"> Sign In</span>
-                </Link>
-            </h2>
-          </CardFooter>
-        </Card>   
+            </div>
+          </form>
+        </CardContent>
+
+        <CardFooter className="flex-col gap-2">
+          <Button type="submit" className="w-full py-5 cursor-pointer bg-[var(--secondaryTheme)]">
+            Register
+          </Button>
+          <Button variant="outline" className="w-full flex items-center gap-2 cursor-pointer py-5">
+            <FcGoogle />
+            Continue with Google
+          </Button>
+          <Button variant="outline" className="w-full cursor-pointer flex items-center gap-2 py-5">
+            <FaFacebook />
+            Continue with Facebook
+          </Button>
+
+          {/* Link to sign up  */}
+          <h2 className="mt-3">Already got an account? 
+              <Link href="/login">
+                <span className="text-[var(--primaryTheme)] font-[700]"> Sign In</span>
+              </Link>
+          </h2>
+        </CardFooter>
+      </Card>   
     )
 }
 
